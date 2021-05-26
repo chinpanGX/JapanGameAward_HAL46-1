abdda9ad2afc5073f1a2fd82eb25678cfcd882ab
ChinpanGX <xchuhe@gameil.com>
2021-05-26 17:19:11 +0900

    ステージ追加

--- GameAward_Prototype/Assets/Script/Water.cs
+++ GameAward_Prototype/Assets/Script/Water.cs
@@ -45,7 +45,7 @@ public class Water : MonoBehaviour
             m_warning.SetActive(false);
             m_flamecount = 0;
             moveflag = false;
-            Fade.FadeOut("SampleScene");
+            Fade.FadeOut("Title");
         }
 
         if (moveflag == true && m_Player.transform.position.y < 20)
