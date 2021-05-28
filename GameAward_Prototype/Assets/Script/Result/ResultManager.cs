5ab1e20371103b385785a8b4f341838416ed4fb7
ChinpanGX <xchuhe@gameil.com>
2021-05-29 03:23:51 +0900

    １２ステージの遷移のバグを修正

--- GameAward_Prototype/Assets/Script/Result/ResultManager.cs
+++ GameAward_Prototype/Assets/Script/Result/ResultManager.cs
@@ -79,7 +79,12 @@ public class ResultManager : MonoBehaviour
             }
             else if (Input.GetKeyDown("joystick button 1") || Input.GetKeyDown(KeyCode.B))//リトライ
             {
-                if (StatusFlagManager.SelectStageID < StatusFlagManager.StageMaxNum - 1)
+
+                if (StatusFlagManager.SelectStageID >= StatusFlagManager.StageMaxNum - 1)//現在のステージが最大の場合
+                {
+                    StatusFlagManager.SelectStageID = StatusFlagManager.StageMaxNum - 1;
+                }
+                if (StatusFlagManager.SelectStageID <=  StatusFlagManager.StageMaxNum - 1)
                 {
                     StatusFlagManager.SceneFlag = StatusFlagManager.SCENE_GAME;
                     StatusFlagManager.GameStatusFlag = StatusFlagManager.GAME_START;
@@ -176,8 +181,8 @@ public class ResultManager : MonoBehaviour
 
             if (StatusFlagManager.SelectStageID >= StatusFlagManager.StageMaxNum - 1)//現在のステージが最大の場合
             {
-                canvas.transform.Find("A").Find("Atext").GetComponent<Text>().text = "Title";
-                canvas.transform.Find("B").gameObject.SetActive(false);
+                canvas.transform.Find("A").Find("Atext").GetComponent<Text>().text = "TiTle";
+                canvas.transform.Find("B").Find("Btext").GetComponent<Text>().text = "Retry";
             }
 
 
